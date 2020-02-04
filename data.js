0eb927e704cf5fef27d02651ebc466748884ac2a
RoshanShrestha123 <roshan.shrestha4u@gmail.com>
2020-02-04 13:53:04 +0545

    node module deleted

--- data.js
+++
@@ -1,5 +0,0 @@
-const data = [
-    {task:"make todo list in web component",completed:false},
-    {task:"now try making it again in polymer",completed:false},
-    {task:"now you can rest",completed:false}
-];
